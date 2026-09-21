c61a464cd09d18eec42129587c77018d9d71af35
Dasturchi-top-1 <sulhiyahasanova1@gmail.com>
2026-09-21 14:52:48 +0500

    Yangilandi: Ertak_chi.py

--- Ertak_chi.py
+++ Ertak_chi.py
@@ -14,7 +14,7 @@ import requests
 # ==========================================================
 # SOZLAMALAR - BU YERGA O'Z API KALITINGIZNI QO'YING
 # ==========================================================
-OPENROUTER_API_KEY = "YOUR_OPENROUTER_KEY"
+OPENROUTER_API_KEY = "YOUR_API_KEY"
 AI_MODEL = "deepseek/deepseek-v4-flash"
 
 SAVE_FOLDER = "/storage/emulated/0/Ertaklar"
